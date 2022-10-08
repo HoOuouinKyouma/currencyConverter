76f702dd1c8f70c5bbd9456e8ee854474ef54bbc
HoOuouinKyouma <fkmll2121@gmail.com>
2022-10-08 14:36:26 +0300

    Update ui.js
    constructor method added

--- ui.js
+++ ui.js
@@ -1,18 +1,26 @@
 class Ui {
+    constructor() {
+        this.amount = document.getElementById("amount");
+
+        this.firstCurrency = document.getElementById("firstCurrency");
+        this.secondCurrency = document.getElementById("secondCurrency");
+
+        this.resultInput = document.getElementById("result-input");
+
+        this.span1 = document.getElementById("span-1");
+        this.span2 = document.getElementById("span-2");
+    }
     getAmout() {
-        return document.getElementById("amount").value;
+        return this.amount.value;
     }
     getOptions() {
-        return [
-            document.getElementById("firstCurrency").value,
-            document.getElementById("secondCurrency").value,
-        ];
+        return [this.firstCurrency.value, this.secondCurrency.value];
     }
     setResult(result) {
-        document.getElementById("result-input").value = result;
+        this.resultInput.value = result;
     }
     setSpanUi(span1, span2) {
-        document.getElementById("span-1").innerHTML = span1;
-        document.getElementById("span-2").innerHTML = span2;
+        this.span1.innerHTML = span1;
+        this.span2.innerHTML = span2;
     }
 }
